ab007b4e2d05478cb9f05b29f455a537c5a3cc18
Daniel <daniel.mainberger@gmail.com>
2019-03-22 11:52:53 +0100

    Aufgabe 0 Push

---
+++ Aufgabe 0/prompter.ts
@@ -0,0 +1,8 @@
+function FunktionPrompter() {
+    var person = prompt("Bitte fügen Sie Ihren Namen ein", "Name");
+    if (person != null) {
+      document.getElementById("PromptText").innerHTML =
+      "Hallo " + person + "! Ich hoffe Du hast einen schönen Tag !";
+    }
+    console.info ("Hallo " + person + "! Ich hoffe Du hast einen schönen Tag !");
+  }
