86512303c98bb8ccf9e04ac3245d458d481979e6
Daniel <daniel.mainberger@gmail.com>
2019-04-14 16:31:24 +0200

    Upload Aufgabe_3

--- Aufgabe_2/js/main.js
+++ Aufgabe_2/js/main.js
@@ -1,148 +1,215 @@
 let cr7 = {
     Symbol: 1,
     Wert: 7,
+    location: "empty",
 };
 let cr8 = {
     Symbol: 1,
     Wert: 8,
+    location: "empty",
 };
 let cr9 = {
     Symbol: 1,
     Wert: 9,
+    location: "empty",
 };
 let cr10 = {
     Symbol: 1,
     Wert: 10,
+    location: "empty",
 };
 let crJ = {
     Symbol: 1,
     Wert: 11,
+    location: "empty",
 };
 let crQ = {
     Symbol: 1,
     Wert: 12,
+    location: "empty",
 };
 let crK = {
     Symbol: 1,
     Wert: 13,
+    location: "empty",
 };
 let crA = {
     Symbol: 1,
     Wert: 14,
+    location: "empty",
 };
 let di7 = {
     Symbol: 2,
     Wert: 7,
+    location: "empty",
 };
 let di8 = {
     Symbol: 2,
     Wert: 8,
+    location: "empty",
 };
 let di9 = {
     Symbol: 2,
     Wert: 9,
+    location: "empty",
 };
 let di10 = {
     Symbol: 2,
     Wert: 10,
+    location: "empty",
 };
 let diJ = {
     Symbol: 2,
     Wert: 11,
+    location: "empty",
 };
 let diQ = {
     Symbol: 2,
     Wert: 12,
+    location: "empty",
 };
 let diK = {
     Symbol: 2,
     Wert: 13,
+    location: "empty",
 };
 let diA = {
     Symbol: 2,
     Wert: 14,
+    location: "empty",
 };
 let sp7 = {
     Symbol: 3,
     Wert: 7,
+    location: "empty",
 };
 let sp8 = {
     Symbol: 3,
     Wert: 8,
+    location: "empty",
 };
 let sp9 = {
     Symbol: 3,
     Wert: 9,
+    location: "empty",
 };
 let sp10 = {
     Symbol: 3,
     Wert: 10,
+    location: "empty",
 };
 let spJ = {
     Symbol: 3,
     Wert: 11,
+    location: "empty",
 };
 let spQ = {
     Symbol: 3,
     Wert: 12,
+    location: "empty",
 };
 let spK = {
     Symbol: 3,
     Wert: 13,
+    location: "empty",
 };
 let spA = {
     Symbol: 3,
     Wert: 14,
+    location: "empty",
 };
 let he7 = {
     Symbol: 4,
     Wert: 7,
+    location: "empty",
 };
 let he8 = {
     Symbol: 4,
     Wert: 8,
+    location: "empty",
 };
 let he9 = {
     Symbol: 4,
     Wert: 9,
+    location: "empty",
 };
 let he10 = {
     Symbol: 4,
     Wert: 10,
+    location: "empty",
 };
 let heJ = {
     Symbol: 4,
     Wert: 11,
+    location: "empty",
 };
 let heQ = {
     Symbol: 4,
     Wert: 12,
+    location: "empty",
 };
 let heK = {
     Symbol: 4,
     Wert: 13,
+    location: "empty",
 };
 let heA = {
     Symbol: 4,
     Wert: 14,
+    location: "empty",
 };
 let Deck = [cr7, cr8, cr9, cr10, crJ, crQ, crK, crA, di7, di8, di9, di10, diJ, diQ, diK, diA, sp7, sp8, sp9, sp10, spJ, spQ, spK, spA, he7, he8, he9, he10, heJ, heQ, heK, heA];
 let Hand = [];
+let Ablagestapel = [];
 let obersteKarte;
 function deckLaden() {
+    document.getElementById("Deck").addEventListener("click", karteZiehen);
     document.getElementById("Deck").innerHTML = `<div class="KartenRand">
     <img src="img/cbg.jpeg" alt="Background Card" class="KartenRücken">
     </div>`;
 }
+function kartenSortieren() {
+    Hand.sort(wertSortieren);
+    Hand.sort(bildSortieren);
+    handBlattErzeugen();
+}
+function wertSortieren(_a, _b) {
+    let value_a = _a.Wert;
+    let value_b = _b.Wert;
+    if (value_a < value_b)
+        return -1;
+    if (value_a > value_b)
+        return 1;
+    if (value_a == value_b)
+        return 0;
+}
+function bildSortieren(_a, _b) {
+    let pic_a = _a.Symbol;
+    let pic_b = _b.Symbol;
+    if (pic_a < pic_b)
+        return -1;
+    if (pic_a > pic_b)
+        return 1;
+    if (pic_a == pic_b)
+        return 0;
+}
+function welcheTaste(event) {
+    if (event.keyCode == 32)
+        karteZiehen();
+}
 function karteZiehen() {
-    let n = Math.floor(Math.random() * (Deck.length - 1)); //Zufallsgenerator und prüfung auf Wiederholung
-    Hand.push(Deck[n]); // Karte -> Hand
-    Deck.splice(n, 1);
+    if (Deck.length > 0) {
+        let n = Math.floor(Math.random() * (Deck.length)); //Zufallsgenerator und prüfung auf Wiederholung
+        Hand.push(Deck[n]); // Karte -> Hand
+        Deck.splice(n, 1);
+        handBlattErzeugen();
+        console.log(Hand);
+    }
+    else {
+        alert("Es gibt keine Karten mehr zum ziehen !");
+    }
 }
 function stapelErzeugen() {
-    let n = Math.floor(Math.random() * (Deck.length - 1)); //Zufallsgenerator und prüfung auf Wiederholung
-    Deck.splice(n, 1);
-    obersteKarte = Deck[n];
     let write = "";
     write += `<div class="KartenRand">`;
     switch (obersteKarte.Symbol) {
@@ -197,10 +264,12 @@ function stapelErzeugen() {
     document.getElementById("Stapel").innerHTML = `${write}`;
 }
 function handBlattErzeugen() {
+    document.getElementById("HandBlatt").addEventListener("click", spieleKarte);
     document.getElementById("HandBlatt").innerHTML = "";
-    for (var i = 0; i < Hand.length; i++) {
+    for (let i = 0; i < Hand.length; i++) {
+        Hand[i].location = "location" + i;
         let write = "";
-        write += `<div class="KartenRand">`;
+        write += `<div class="KartenRand" id="location${i}">`;
         switch (Hand[i].Symbol) {
             case 1:
                 write += `<img src="img/kreuz.png" class="Symbol" alt="♣">
@@ -253,6 +322,24 @@ function handBlattErzeugen() {
         document.getElementById("HandBlatt").innerHTML += `${write}`;
     }
 }
+function spieleKarte() {
+    let ausgewählteKartenID = event.target;
+    for (let i = 0; i < handBlattErzeugen.length; i++) {
+        if (String(ausgewählteKartenID.getAttribute("id")) == Hand[i].location) {
+            if (Hand[i].Symbol == obersteKarte.Symbol || Hand[i].Wert == obersteKarte.Wert) {
+                Ablagestapel.push(obersteKarte);
+                obersteKarte = Hand[i];
+                Hand[i].location = "Empty";
+                Hand.splice(i, 1);
+                handBlattErzeugen();
+                stapelErzeugen();
+            }
+            else {
+                alert("Diese Karte darf nicht abgelegt werden!");
+            }
+        }
+    }
+}
 function startBlatt() {
     let handAnzahl = 0;
     do {
@@ -264,8 +351,12 @@ function startBlatt() {
     }
     console.log(Hand);
     deckLaden();
+    let n = Math.floor(Math.random() * (Deck.length)); //Zufallsgenerator und prüfung auf Wiederholung
+    Deck.splice(n, 1);
+    obersteKarte = Deck[n];
     stapelErzeugen();
     handBlattErzeugen();
+    document.getElementById("Sortieren").addEventListener("click", kartenSortieren);
 }
 function init() {
     startBlatt();
--- Aufgabe_2/js/main.ts
+++ Aufgabe_2/js/main.ts
@@ -1,258 +1,328 @@
 interface Karten {
-    Symbol:number; // 1 = Kreuz, 2 = Karo, 3 = Pik, 4 = Herz
-    Wert:number;
+    Symbol: number; // 1 = Kreuz, 2 = Karo, 3 = Pik, 4 = Herz
+    Wert: number;
+    location: string;
 }
 
-let cr7:Karten={ //Kreuz
-    Symbol:1,
-    Wert:7,   
+let cr7: Karten = { //Kreuz
+    Symbol: 1,
+    Wert: 7,
+    location: "empty",
 }
 
-let cr8:Karten={
-    Symbol:1,
-    Wert:8,   
+let cr8: Karten = {
+    Symbol: 1,
+    Wert: 8,
+    location: "empty",
 }
 
-let cr9:Karten={
-    Symbol:1,
-    Wert:9,   
+let cr9: Karten = {
+    Symbol: 1,
+    Wert: 9,
+    location: "empty",
 }
 
-let cr10:Karten={
-    Symbol:1,
-    Wert:10,   
+let cr10: Karten = {
+    Symbol: 1,
+    Wert: 10,
+    location: "empty",
 }
 
-let crJ:Karten={
-    Symbol:1,
-    Wert:11,   
+let crJ: Karten = {
+    Symbol: 1,
+    Wert: 11,
+    location: "empty",
 }
 
-let crQ:Karten={
-    Symbol:1,
-    Wert:12,   
+let crQ: Karten = {
+    Symbol: 1,
+    Wert: 12,
+    location: "empty",
 }
 
-let crK:Karten={
-    Symbol:1,
-    Wert:13,   
+let crK: Karten = {
+    Symbol: 1,
+    Wert: 13,
+    location: "empty",
 }
 
-let crA:Karten={
-    Symbol:1,
-    Wert:14,   
+let crA: Karten = {
+    Symbol: 1,
+    Wert: 14,
+    location: "empty",
 }
 
-let di7:Karten={ //Karo
-    Symbol:2,
-    Wert:7,   
+let di7: Karten = { //Karo
+    Symbol: 2,
+    Wert: 7,
+    location: "empty",
 }
 
-let di8:Karten={
-    Symbol:2,
-    Wert:8,   
+let di8: Karten = {
+    Symbol: 2,
+    Wert: 8,
+    location: "empty",
 }
 
-let di9:Karten={
-    Symbol:2,
-    Wert:9,   
+let di9: Karten = {
+    Symbol: 2,
+    Wert: 9,
+    location: "empty",
 }
 
-let di10:Karten={
-    Symbol:2,
-    Wert:10,   
+let di10: Karten = {
+    Symbol: 2,
+    Wert: 10,
+    location: "empty",
 }
 
-let diJ:Karten={
-    Symbol:2,
-    Wert:11,   
+let diJ: Karten = {
+    Symbol: 2,
+    Wert: 11,
+    location: "empty",
 }
 
-let diQ:Karten={
-    Symbol:2,
-    Wert:12,   
+let diQ: Karten = {
+    Symbol: 2,
+    Wert: 12,
+    location: "empty",
 }
 
-let diK:Karten={
-    Symbol:2,
-    Wert:13,   
+let diK: Karten = {
+    Symbol: 2,
+    Wert: 13,
+    location: "empty",
 }
 
-let diA:Karten={
-    Symbol:2,
-    Wert:14,   
+let diA: Karten = {
+    Symbol: 2,
+    Wert: 14,
+    location: "empty",
 }
 
-let sp7:Karten={ //Pik
-    Symbol:3,
-    Wert:7,   
+let sp7: Karten = { //Pik
+    Symbol: 3,
+    Wert: 7,
+    location: "empty",
 }
 
-let sp8:Karten={
-    Symbol:3,
-    Wert:8,   
+let sp8: Karten = {
+    Symbol: 3,
+    Wert: 8,
+    location: "empty",
 }
 
-let sp9:Karten={
-    Symbol:3,
-    Wert:9,   
+let sp9: Karten = {
+    Symbol: 3,
+    Wert: 9,
+    location: "empty",
 }
 
-let sp10:Karten={
-    Symbol:3,
-    Wert:10,   
+let sp10: Karten = {
+    Symbol: 3,
+    Wert: 10,
+    location: "empty",
 }
 
-let spJ:Karten={
-    Symbol:3,
-    Wert:11,   
+let spJ: Karten = {
+    Symbol: 3,
+    Wert: 11,
+    location: "empty",
 }
 
-let spQ:Karten={
-    Symbol:3,
-    Wert:12,   
+let spQ: Karten = {
+    Symbol: 3,
+    Wert: 12,
+    location: "empty",
 }
 
-let spK:Karten={
-    Symbol:3,
-    Wert:13,   
+let spK: Karten = {
+    Symbol: 3,
+    Wert: 13,
+    location: "empty",
 }
 
-let spA:Karten={
-    Symbol:3,
-    Wert:14,   
+let spA: Karten = {
+    Symbol: 3,
+    Wert: 14,
+    location: "empty",
 }
 
-let he7:Karten={ //Herz
-    Symbol:4,
-    Wert:7,   
+let he7: Karten = { //Herz
+    Symbol: 4,
+    Wert: 7,
+    location: "empty",
 }
 
-let he8:Karten={
-    Symbol:4,
-    Wert:8,   
+let he8: Karten = {
+    Symbol: 4,
+    Wert: 8,
+    location: "empty",
 }
 
-let he9:Karten={
-    Symbol:4,
-    Wert:9,   
+let he9: Karten = {
+    Symbol: 4,
+    Wert: 9,
+    location: "empty",
 }
 
-let he10:Karten={
-    Symbol:4,
-    Wert:10,   
+let he10: Karten = {
+    Symbol: 4,
+    Wert: 10,
+    location: "empty",
 }
 
-let heJ:Karten={
-    Symbol:4,
-    Wert:11,   
+let heJ: Karten = {
+    Symbol: 4,
+    Wert: 11,
+    location: "empty",
 }
 
-let heQ:Karten={
-    Symbol:4,
-    Wert:12,   
+let heQ: Karten = {
+    Symbol: 4,
+    Wert: 12,
+    location: "empty",
 }
 
-let heK:Karten={
-    Symbol:4,
-    Wert:13,   
+let heK: Karten = {
+    Symbol: 4,
+    Wert: 13,
+    location: "empty",
 }
 
-let heA:Karten={
-    Symbol:4,
-    Wert:14,   
+let heA: Karten = {
+    Symbol: 4,
+    Wert: 14,
+    location: "empty",
 }
 
-let Deck:Karten[]=[cr7, cr8, cr9, cr10, crJ, crQ, crK, crA, di7, di8, di9, di10, diJ, diQ, diK, diA, sp7, sp8, sp9, sp10, spJ, spQ, spK, spA, he7, he8, he9, he10, heJ, heQ, heK, heA];
+let Deck: Karten[] = [cr7, cr8, cr9, cr10, crJ, crQ, crK, crA, di7, di8, di9, di10, diJ, diQ, diK, diA, sp7, sp8, sp9, sp10, spJ, spQ, spK, spA, he7, he8, he9, he10, heJ, heQ, heK, heA];
 
-let Hand:Karten[]=[];
+let Hand: Karten[] = [];
+let Ablagestapel: Karten[] = [];
 
-let obersteKarte:Karten;
+let obersteKarte: Karten;
 
 
-function deckLaden():void{
+function deckLaden(): void {
+    document.getElementById("Deck").addEventListener("click", karteZiehen)
     document.getElementById("Deck").innerHTML = `<div class="KartenRand">
     <img src="img/cbg.jpeg" alt="Background Card" class="KartenRücken">
-    </div>`;     
+    </div>`;
 }
 
 
-function karteZiehen():void {
-    let n:number = Math.floor(Math.random() * (Deck.length)); //Zufallsgenerator und prüfung auf Wiederholung
-    Hand.push(Deck[n]); // Karte -> Hand
-    Deck.splice(n,1)  
+
+function kartenSortieren(){
+    Hand.sort(wertSortieren);
+    Hand.sort(bildSortieren);
+    handBlattErzeugen();
 }
 
-function stapelErzeugen():void{
-    let n:number = Math.floor(Math.random() * (Deck.length)); //Zufallsgenerator und prüfung auf Wiederholung
-    Deck.splice(n,1);  
-    obersteKarte=Deck[n];  
-    
-    let write:string = "";
-        write += `<div class="KartenRand">`;
+function wertSortieren(_a: Karten, _b: Karten): number {
+    let value_a: number = _a.Wert;
+    let value_b: number = _b.Wert;
+    if (value_a < value_b) return -1;
+    if (value_a > value_b) return 1;
+    if (value_a == value_b) return 0;
+}
 
-        switch (obersteKarte.Symbol){
-            case 1:
-                write += `<img src="img/kreuz.png" class="Symbol" alt="♣">
+function bildSortieren(_a: Karten, _b: Karten): number {
+    let pic_a: number = _a.Symbol;
+    let pic_b: number = _b.Symbol;
+    if (pic_a < pic_b) return -1;
+    if (pic_a > pic_b) return 1;
+    if (pic_a == pic_b) return 0;
+}
+
+
+function welcheTaste(event: KeyboardEvent): void {
+    if (event.keyCode == 32) karteZiehen();
+}
+
+
+function karteZiehen(): void {
+    if (Deck.length > 0) {
+        let n: number = Math.floor(Math.random() * (Deck.length)); //Zufallsgenerator und prüfung auf Wiederholung
+        Hand.push(Deck[n]); // Karte -> Hand
+        Deck.splice(n, 1)
+        handBlattErzeugen();
+        console.log(Hand);
+    }
+    else {
+        alert("Es gibt keine Karten mehr zum ziehen !")
+    }
+}
+
+function stapelErzeugen(): void {
+
+    let write: string = "";
+    write += `<div class="KartenRand">`;
+
+    switch (obersteKarte.Symbol) {
+        case 1:
+            write += `<img src="img/kreuz.png" class="Symbol" alt="♣">
                 <div class="Schwarz">`; // = ♣
-                break;
-            case 2:
-                write += `<img src="img/caro.png" class="Symbol" alt="♦">
+            break;
+        case 2:
+            write += `<img src="img/caro.png" class="Symbol" alt="♦">
                 <div class="Rot">`; // = ♦
-                break;
-            case 3:
-                write += `<img src="img/pick.png" class="Symbol" alt="♠">
+            break;
+        case 3:
+            write += `<img src="img/pick.png" class="Symbol" alt="♠">
                 <div class="Schwarz">`; // = ♠
-                break;
-            case 4:
-                write += `<img src="img/herz.png" class="Symbol" alt="♥">
+            break;
+        case 4:
+            write += `<img src="img/herz.png" class="Symbol" alt="♥">
                 <div class="Rot">`; // = ♥
-                break;        
-            default:
-                console.log("Fehler beim Symbol laden")
-            }
+            break;
+        default:
+            console.log("Fehler beim Symbol laden")
+    }
 
-        switch (obersteKarte.Wert){
-            case 7:
-                write += `7</div>`;
-                break;
-            case 8:
-                write += `8</div>`;
-                break;
-            case 9:
-                write += `9</div>`;
-                break;
-            case 10:
-                write += `10</div>`;
-                break;        
-            case 11:
-                write += `J</div>`; // 11 = Bube (J)
-                break;  
-            case 12:
-                write += `Q</div>`; // 12 = Dame (Q)
-                break;          
-            case 13:
-                write += `K</div>`; // 13 = König (K)
-                break;          
-            case 14:
-                write += `A</div>`; // 14 = Ass (A)
-                break;
-            default:
-                console.log("Fehler beim Werte laden")
-            }
-            write += `</div>`
-    document.getElementById("Stapel").innerHTML = `${write}`;     
+    switch (obersteKarte.Wert) {
+        case 7:
+            write += `7</div>`;
+            break;
+        case 8:
+            write += `8</div>`;
+            break;
+        case 9:
+            write += `9</div>`;
+            break;
+        case 10:
+            write += `10</div>`;
+            break;
+        case 11:
+            write += `J</div>`; // 11 = Bube (J)
+            break;
+        case 12:
+            write += `Q</div>`; // 12 = Dame (Q)
+            break;
+        case 13:
+            write += `K</div>`; // 13 = König (K)
+            break;
+        case 14:
+            write += `A</div>`; // 14 = Ass (A)
+            break;
+        default:
+            console.log("Fehler beim Werte laden")
+    }
+    write += `</div>`
+    document.getElementById("Stapel").innerHTML = `${write}`;
 }
 
-function handBlattErzeugen():void{
+function handBlattErzeugen(): void {
 
+    document.getElementById("HandBlatt").addEventListener("click", spieleKarte);
     document.getElementById("HandBlatt").innerHTML = "";
 
-    for (var i: number = 0; i < Hand.length; i++){
-        let write:string = "";
-        write += `<div class="KartenRand">`;
+    for (let i: number = 0; i < Hand.length; i++) {
+        Hand[i].location = "location" + i;
+        let write: string = "";
+        write += `<div class="KartenRand" id="location${i}">`;
 
-        switch (Hand[i].Symbol){
+        switch (Hand[i].Symbol) {
             case 1:
                 write += `<img src="img/kreuz.png" class="Symbol" alt="♣">
                 <div class="Black">`; // = ♣
@@ -268,12 +338,12 @@ function handBlattErzeugen():void{
             case 4:
                 write += `<img src="img/herz.png" class="Symbol" alt="♥">
                 <div class="Red">`; // = ♥
-                break;        
+                break;
             default:
                 console.log("Fehler beim Symbol laden")
-            }
+        }
 
-        switch (Hand[i].Wert){
+        switch (Hand[i].Wert) {
             case 7:
                 write += `7</div>`;
                 break;
@@ -285,45 +355,73 @@ function handBlattErzeugen():void{
                 break;
             case 10:
                 write += `10</div>`;
-                break;        
+                break;
             case 11:
                 write += `J</div>`; // 11 = Bube (J)
-                break;  
+                break;
             case 12:
                 write += `Q</div>`; // 12 = Dame (Q)
-                break;          
+                break;
             case 13:
                 write += `K</div>`; // 13 = König (K)
-                break;          
+                break;
             case 14:
                 write += `A</div>`; // 14 = Ass (A)
                 break;
             default:
                 console.log("Fehler beim Wert laden")
-            }
-            write += `</div>`
+        }
+        write += `</div>`
         document.getElementById("HandBlatt").innerHTML += `${write}`;
-    }       
+    }
 }
 
-function startBlatt():void {
+function spieleKarte(): void {
+    let ausgewählteKartenID: HTMLElement = <HTMLElement>event.target;
+    for (let i = 0; i < handBlattErzeugen.length; i++) {
+        if (String(ausgewählteKartenID.getAttribute("id")) == Hand[i].location) {
+            if (Hand[i].Symbol == obersteKarte.Symbol || Hand[i].Wert == obersteKarte.Wert) {
+                Ablagestapel.push(obersteKarte);
+                obersteKarte = Hand[i];
+                Hand[i].location = "Empty";
+                Hand.splice(i, 1);
+                handBlattErzeugen();
+                stapelErzeugen();
+
+            }
+            else {
+                alert("Diese Karte darf nicht abgelegt werden!")
+            }
+        }
+    }
+}
+
+
+
+
+function startBlatt(): void {
     let handAnzahl: number = 0;
-    do  
-        {
-            handAnzahl = parseInt(prompt("Karten pro Hand (Wert zwischen 4-8)"));
-        } 
+    do {
+        handAnzahl = parseInt(prompt("Karten pro Hand (Wert zwischen 4-8)"));
+    }
     while (isNaN(handAnzahl) || handAnzahl > 8 || handAnzahl < 4); //Wiederholung bis Wert Vorgaben entspricht.
 
-    console.log("HandAnzahl: "+handAnzahl);
+    console.log("HandAnzahl: " + handAnzahl);
 
-    for(let i:number=0;i<handAnzahl;i++){
+    for (let i: number = 0; i < handAnzahl; i++) {
         karteZiehen();
     }
     console.log(Hand);
 
     deckLaden();
+
+    let n: number = Math.floor(Math.random() * (Deck.length)); //Zufallsgenerator und prüfung auf Wiederholung
+    Deck.splice(n, 1);
+    obersteKarte = Deck[n];
+
     stapelErzeugen();
     handBlattErzeugen();
+    document.getElementById("Sortieren").addEventListener("click", kartenSortieren)
 }
 
 
@@ -331,5 +429,9 @@ function startBlatt():void {
 function init() {
     startBlatt();
 }
+
+
+
+
 
 document.addEventListener("DOMContentLoaded", init);
